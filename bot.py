cf5a3f457099dd8338f1d535dbf1009f1245cd6e
Maxim Zavadskiy <maxim.zavadskiy@medisapiens.fi>
2018-04-22 15:46:36 +0300

    Fix

--- bot.py
+++ bot.py
@@ -2,14 +2,15 @@ from rocketchat_API.rocketchat import RocketChat
 
 ###VARIABLES THAT YOU NEED TO SET MANUALLY IF NOT ON HEROKU#####
 try:
-        SERVER_URL = os.environ['SERVER_URL']
-        BOT_USERNAME = os.environ['BOT_USERNAME']
-        BOT_PASS = os.environ['BOT_PASS']
-        MESSAGE = os.environ['WELCOME_MESSAGE']
+    SERVER_URL = os.environ['SERVER_URL']
+    BOT_USERNAME = os.environ['BOT_USERNAME']
+    BOT_PASS = os.environ['BOT_PASS']
+    MESSAGE = os.environ['WELCOME_MESSAGE']
 except:
-        BOT_USERNAME = 'Manually set BOT_USERNAME if youre not running through heroku or have not set vars in ENV'
-        BOT_PASS = 'Manually set BOT_PASS if youre not running through heroku or have not set vars in ENV'
-        MESSAGE = 'Manually set the MESSAGE if youre not running through heroku or have not set vars in ENV'
+    SERVER_URL = 'Manually set SERVER_URL if youre not running through heroku or have not set vars in ENV'
+    BOT_USERNAME = 'Manually set BOT_USERNAME if youre not running through heroku or have not set vars in ENV'
+    BOT_PASS = 'Manually set BOT_PASS if youre not running through heroku or have not set vars in ENV'
+    MESSAGE = 'Manually set the MESSAGE if youre not running through heroku or have not set vars in ENV'
 ###############################################################
 
 
